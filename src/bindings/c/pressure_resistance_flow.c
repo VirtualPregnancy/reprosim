bfe70293cdf30040b15fd13cdb2fdf132da4be7a
Your Name <alys.clark@auckland.ac.nz>
2021-09-07 14:53:13 +1200

    correcting bindings for orq

--- src/bindings/c/pressure_resistance_flow.c
+++ src/bindings/c/pressure_resistance_flow.c
@@ -2,7 +2,7 @@
 #include "string.h"
 
 void calculate_stats_c(const char *FLOW_GEN_FILE, int *filename_len, double *image_voxel_size, int *output_level);
-void evaluate_prq(const char *mesh_type, const char *bc_type, const char *rheology_type, const char *vessel_type, double inlet_flow,double inlet_pressure, double outlet_pressure);
+void evaluate_prq_c(const char *mesh_type, int *mesh_type_len, const char *bc_type, int *bc_type_len, const char *rheology_type, int *rheology_type_len, const char *vessel_type, int *vessel_type_len, double *inlet_flow,double *inlet_pressure, double *outlet_pressure);
 
 void calculate_stats(const char *FLOW_GEN_FILE, double image_voxel_size, int output_level)
 {
