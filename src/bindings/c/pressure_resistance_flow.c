bea6b7ca387d22c92db283bb280fc9ab34fd3457
Your Name <alys.clark@auckland.ac.nz>
2021-09-07 14:46:20 +1200

    correcting bindings

--- src/bindings/c/pressure_resistance_flow.c
+++ src/bindings/c/pressure_resistance_flow.c
@@ -1,7 +1,7 @@
 #include "pressure_resistance_flow.h"
 #include "string.h"
 
-void calculate_stats_c(const char *FLOW_GEN_FILE, int *filename_len, double *image_voxel_size, int output_level);
+void calculate_stats_c(const char *FLOW_GEN_FILE, int *filename_len, double *image_voxel_size, int *output_level);
 void evaluate_prq(const char *mesh_type, const char *bc_type, const char *rheology_type, const char *vessel_type, double inlet_flow,double inlet_pressure, double outlet_pressure);
 
 void calculate_stats(const char *FLOW_GEN_FILE, double image_voxel_size, int output_level)
